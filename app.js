5895983f8213996b04817081bb979e6a58fc9112
hassejaharsha-cell <hasseja.harsha@gmail.com>
2026-03-05 18:10:20 +0530

    Update response message in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello from Jenkins CI/CD Pipeline 🚀");
+    res.send("Hello from Jenkins CI/CD Dev Pipeline");
 });
 
 app.listen(3000, () => {
